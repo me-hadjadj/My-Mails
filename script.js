3c5fc93a74e5770f37ae9cfd41548a0597489857
Mehdi Hadjadj <mehdi.hadjadj@outlook.fr>
2022-07-03 14:54:35 +0200

    Mise en place du compteur & fonctionnalité de suppression de message

---
+++ script.js
@@ -0,0 +1,13 @@
+var messagesCount = document.getElementsByTagName('p').length;
+document.getElementById('count').textContent = messagesCount;
+
+for(var i=0; i<document.getElementsByClassName('trash').length; i++){
+    document.getElementsByClassName('trash')[i].addEventListener("click",
+    function(){
+        this.parentNode.remove();
+        var messagesCount = document.getElementsByTagName('p').length;
+        document.getElementById('count').textContent = messagesCount;
+    }
+    )
+
+}
