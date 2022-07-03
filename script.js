440d13b029223a8a23d3efbec59e985289bfb734
Mehdi Hadjadj <mehdi.hadjadj@outlook.fr>
2022-07-03 15:08:31 +0200

    Refonte technique complète du projet avec jQuery et ajout de la fonctionnalité de recherche d’un message par nom d’utilisateur

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
-var messagesCount = document.getElementsByTagName('p').length;
-document.getElementById('count').textContent = messagesCount;
+var messagesCount = $('p').length;
+$('#count').text(messagesCount);
 
-for(var i=0; i<document.getElementsByClassName('trash').length; i++){
-    document.getElementsByClassName('trash')[i].addEventListener("click",
-    function(){
-        this.parentNode.remove();
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
-    }
-    )
+$("body").on("click", '.trash', function(){
+    $(this).parent().remove();
+    var messagesCount = $('p').length;
+    $('#count').text(messagesCount);
+})
 
-}
+$('#btn-add').click(function(){
+    var message = $('#add-message').val();
+    $("body").append(`
+                        <div class="row">
+                            <img class="avatar" src="avatar-1.jpg">
+                            <div>
+                                <h6>Eric Dupont</h6>
+                                <p>`+ message + `</p>
+                            </div>
+                            <img class="trash"src="trash.png">
+                        </div>`
 
-document.getElementById('btn-add').addEventListener("click",
-    function(){
-        var mainDiv = document.createElement("div");
-            mainDiv.className = "row";
-            document.body.appendChild(mainDiv);
+    );
+    $('#count').text($('p').length);
+    $('#add-message').val("");
+})
 
-        var image = document.createElement("img");
-            image.className = "avatar";
-            image.src = "avatar-1.jpg";
-            mainDiv.appendChild(image);
-        
-        var nameMessageDiv = document.createElement("div");
-            mainDiv.appendChild(nameMessageDiv);
-
-        var h6 = document.createElement('h6');
-            h6.textContent = "Mehdi Hadjadj";
-            nameMessageDiv.appendChild(h6);
-
-        var content = document.createElement('p');
-            content.textContent = document.getElementById('add-message').value;
-            nameMessageDiv.appendChild(content);
-
-        var trash = document.createElement('img');
-        trash.src = "trash.png";
-        trash.className = "trash";
-        mainDiv.appendChild(trash);
-
-        document.getElementById('add-message').value = "";
-        
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
-
-        
-        trash.addEventListener("click", 
-            function(){
-                this.parentNode.remove();
-                var messagesCount = document.getElementsByTagName('p').length;
-                document.getElementById('count').textContent = messagesCount;
-
-            }
-        )
-    }
-)
+$('#btn-search').click(function(){
+    $('h6').each(function(){
+        if($("#search-message").val() != $(this).text()){
+            $(this).parent().parent().fadeOut();
+        } else {
+            $(this).parent().parent().show();
+        }
+    })
+    $("#search-message").val("");
+})
